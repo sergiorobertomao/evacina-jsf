9636caeb8a5a65121045d22c9c081797365ed439
Fernando Gurgel <fag.gurgel@gmail.com>
2018-10-29 11:50:40 -0400

    Hibernate

--- evacina-jsf/evacina/src/java/br/ifam/model/Vacinacao.java
+++ evacina-jsf/evacina/src/java/br/ifam/model/Vacinacao.java
@@ -1,5 +1,6 @@
 package br.ifam.model;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 @Entity
-public class Vacinacao {
+public class Vacinacao implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO )
