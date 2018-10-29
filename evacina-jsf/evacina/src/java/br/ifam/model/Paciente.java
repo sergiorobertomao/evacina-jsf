4e0b0bf18c28f42b4d8f1355057a6e7e2a2144ad
Fernando Gurgel <fag.gurgel@gmail.com>
2018-10-29 11:12:21 -0400

    model

---
+++ evacina-jsf/evacina/src/java/br/ifam/model/Paciente.java
@@ -0,0 +1,75 @@
+package br.ifam.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+
+
+@Entity
+public class Paciente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private long sus;
+    private String nome;
+    private String sexo;
+    private String nascimento;
+    @OneToMany
+    @JoinColumn(name="sus")
+    private List<Vacinacao> vacinacao;
+
+
+    public Paciente() {
+		// TODO Auto-generated constructor stub
+	}
+
+    public Paciente(String nome, String sexo, String nascimento) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nascimento = nascimento;
+    }
+
+    
+    
+    public Paciente(long sus, String nome, String sexo, String nascimento, List<Vacinacao> vacinacao) {
+		this.sus = sus;
+		this.nome = nome;
+		this.sexo = sexo;
+		this.nascimento = nascimento;
+		this.vacinacao = vacinacao;
+	}
+
+	public long getSus() {
+        return sus;
+    }
+
+    public void setSus(long sus) {
+        this.sus = sus;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+}
---
+++ evacina-jsf/evacina/src/java/br/ifam/model/Vacina.java
@@ -0,0 +1,48 @@
+package br.ifam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vacina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private String descricao;
+
+    public Vacina(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    
+    public Vacina() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
